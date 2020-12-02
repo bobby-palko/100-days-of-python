6114125c67e0baeac9e391d75281065ad2d06259
Bobby Palko <bobby@palko.dev>
2020-12-01 21:30:00 -0500

    day 4: password generator

---
+++ 04/README.md
@@ -0,0 +1,10 @@
+# Day 5 of 100 days of Python
+
+## What this is?
+A "password generator"
+
+## What about python does it showcase?
+It's an introduction to some loops
+
+## How do I run it?
+Clone or download this repo, and just run `python main.py` from inside this folder!
---
+++ 04/main.py
@@ -0,0 +1,31 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+
+# Get requirements from user
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+new_password = []
+
+# Add the letters
+for i in range(nr_letters):
+    new_password.append(random.choice(letters))
+
+# numbers
+for i in range(nr_numbers):
+    new_password.append(random.choice(numbers))
+
+# special characters
+for i in range(nr_symbols):
+    new_password.append(random.choice(symbols))
+
+# shuffle it up
+random.shuffle(new_password)
+
+print(''.join(new_password))
