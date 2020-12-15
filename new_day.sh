2d600c25e7b9266fecbaef4dc52b54b354496eff
Bobby Palko <bobby@palko.dev>
2020-12-15 18:03:05 -0500

    fixed typos

--- new_day.sh
+++ new_day.sh
@@ -1,4 +1,4 @@
-!# /bin/bash
+#! /bin/bash
 
 #Just in case I run this in a different directory
 cd ~/projects/udemy/100-days-of-python
@@ -7,7 +7,7 @@ cd ~/projects/udemy/100-days-of-python
 # (This could be subject to failure if I modify a previous folder)
 last=$(ls -t | egrep '[0-9]{2}' | cut -d$'\n' -f1)
 
-next=$((last+1))
+next=$(($last+1))
 
 # Create a new branch
 git checkout -b $next
@@ -18,5 +18,10 @@ cp $last/README.md $next/
 
 cd $next
 
+# Pad the number by one to reflect counting from 1
+pad=$(($next+1))
+
 # Update the README to say the correct day
-sed -i /$next/$((next+1))/g
+sed -i "s/$next/$pad/g" README.md
+
+return 
