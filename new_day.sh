c860d1095930cf2a033b8167f4623309b6024863
Bobby Palko <bobby@palko.dev>
2020-12-15 17:30:15 -0500

    script to automatically create new branch and folder for the next project

---
+++ new_day.sh
@@ -0,0 +1,22 @@
+!# /bin/bash
+
+#Just in case I run this in a different directory
+cd ~/projects/udemy/100-days-of-python
+
+# Gets the last touched numeric file in the directory
+# (This could be subject to failure if I modify a previous folder)
+last=$(ls -t | egrep '[0-9]{2}' | cut -d$'\n' -f1)
+
+next=$((last+1))
+
+# Create a new branch
+git checkout -b $next
+
+# Make the directory for the day, copy yesterday's README into it
+mkdir $next
+cp $last/README.md $next/
+
+cd $next
+
+# Update the README to say the correct day
+sed -i /$next/$((next+1))/g
